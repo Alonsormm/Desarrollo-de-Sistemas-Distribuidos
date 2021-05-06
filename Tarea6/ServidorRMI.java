125d3e6cfee94f42889c76afca87fbc019f5cf4d
Alonso Maya <alonsorubmaya@gmail.com>
2021-05-06 10:15:06 -0500

    Se agregaron badneras

--- Tarea6/ServidorRMI.java
+++ Tarea6/ServidorRMI.java
@@ -4,10 +4,12 @@ public class ServidorRMI
 {
   public static void main(String[] args) throws Exception
   {
+    System.out.println(0);
     String url = "//localhost/matrices";
     ClaseRMI obj = new ClaseRMI();
-
+    System.out.println(2);
     // registra la instancia en el rmiregistry
     Naming.rebind(url,obj);
+    System.out.println(3);
   }
 }
